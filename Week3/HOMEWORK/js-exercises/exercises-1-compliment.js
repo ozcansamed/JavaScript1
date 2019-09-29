5e20e052c1548453f2a836556d03d8742ac1e223
ozcansamed <ozcansamed61@gmail.com>
2019-09-29 23:58:33 +0200

    create and add exercise1

---
+++ Week3/HOMEWORK/js-exercises/exercises-1-compliment.js
@@ -0,0 +1,31 @@
+'use strict';
+console.log('EXERCISE 1 - You are amazing, ....!');
+
+/* EXERCISE-1 =>
+1=> Create a function with one parameter, 
+2=> An array with 10 strings,
+3=> Use Math.random and Math.floor to pick compliments randomly
+4=> Compose our strings and edit name and compliments uppercase by .toUpperCase method,
+5=> Return the string and log,
+6=> Call 3 times and it worked!!!  */
+
+function giveCompliment(name) {
+  const complimentArr = [
+    'awesome',
+    'spectacular',
+    'superb',
+    'amazing',
+    'glorious',
+    'magnificent',
+    'majestic',
+    'extraordinary',
+    'incredible',
+    'marvelous',
+  ];
+  let randomCompliment = complimentArr[Math.floor(Math.random() * (complimentArr.length - 1))];
+  let complimentText = `You are ${randomCompliment.toLocaleUpperCase()}, ${name.toLocaleUpperCase()}!`;
+  return complimentText;
+}
+console.log(giveCompliment('David'));
+console.log(giveCompliment('Yash'));
+console.log(giveCompliment('all HYF instructors'));
