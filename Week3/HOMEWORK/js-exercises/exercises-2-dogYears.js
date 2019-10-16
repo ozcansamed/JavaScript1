718839cae0ffce3ecbb7a225f3fad7bc5a33818d
ozcansamed <ozcansamed61@gmail.com>
2019-10-16 03:05:56 +0200

    changes for review

--- Week3/HOMEWORK/js-exercises/exercises-2-dogYears.js
+++ Week3/HOMEWORK/js-exercises/exercises-2-dogYears.js
@@ -7,14 +7,12 @@ console.log('EXERCISE 2 - Dog years');
 3=> Return a string,
 4=> Call it 3 times with different values.    */
 
-function calculateDogAge(age) {
-  const convertedAge = age * 7;
-  const dogAgeString = `Your doggie is ${convertedAge} years old in dog years!`;
+function calculateDogAge(humanYears) {
+  const dogYears = humanYears / 7;
+  const dogAgeString = `Your doggie is ${dogYears} years old in dog years!`;
   return dogAgeString;
-  // or we can abbreviate the return part like this:
-  //return 'Your doggie is ' + age * 7 + ' years old in dog years!';
 }
 
-console.log(calculateDogAge(1));
-console.log(calculateDogAge(1.55));
-console.log(calculateDogAge(7.5));
+console.log(calculateDogAge(28));
+console.log(calculateDogAge(42));
+console.log(calculateDogAge(34));
--- Week3/HOMEWORK/js-exercises/exercises-5-totalCost.js
+++ Week3/HOMEWORK/js-exercises/exercises-5-totalCost.js
@@ -7,7 +7,7 @@ console.log('EXERCISE 5 -  Total cost is ...');
 3=> Return the total value,
 4=> Create an object with 5 properties,
 5=> Call the function and log it with a string. */
-
+/*
 function calculateTotalPrice(shoppingCart) {
   let total = 0;
   for (let shoppingItem in shoppingCart) {
@@ -24,3 +24,16 @@ const cartForParty = {
   chocolate: 0.9,
 };
 console.log('Your total cost is ' + calculateTotalPrice(cartForParty) + ' €.');
+*/
+
+const cartForParty = {
+  cola: 3.25,
+  chips: 2.75,
+  gum: 0.25,
+  biscuits: 1.75,
+  chocolate: 0.9,
+};
+const calculateTotalPrice = Object.values(cartForParty).reduce((total, price) => {
+  return total + price;
+}, 0);
+console.log('Your total cost is ' + calculateTotalPrice + ' €.');
